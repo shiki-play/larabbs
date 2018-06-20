965ee2c5a0e03469f409c1684b7bb25b30cb5e09
shiki <1401315109@qq.com>
2018-06-20 11:12:23 +0800

    Adjust configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel{{ config('app.timezone')}}
                 </div>
 
                 <div class="links">
