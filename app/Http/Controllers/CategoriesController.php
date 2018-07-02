6ed4839697ddedd5cd9d6632354b2bb574c8ffd3
shiki <1401315109@qq.com>
2018-07-02 17:50:54 +0800

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,9 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
-        $topics =Topic::where('category_id',$category->id)->with('category','user')
+    public function show(Category $category, Request $request, Topic $topic){
+        $topics =$topic->withOrder($request->order)
+        ->where('category_id',$category->id)
         ->paginate(20);
         return view('topics.index',compact('topics','category'));
     }
