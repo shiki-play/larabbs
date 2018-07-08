5e4eb69a8fd202862f57b888bc9509b2ed2e74f4
shiki <1401315109@qq.com>
2018-07-08 18:56:13 +0800

    Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -31,4 +31,7 @@ class Topic extends Model
     public function scopeRecent($query){
         return $query->orderBy('created_at','desc');
     }
+    public function link($params=[]){
+        return route('topics.show',array_merge([$this->id,$this->slug],$params));
+    }
 }
